5108375973482d080510ad4e5f6ee6b7a91c0189
Vsevolod Pokhvalenko <119959398+xpokhv00@users.noreply.github.com>
2025-04-21 21:50:05 +0200

    minor improvement

--- frontend/src/components/CommunicationGraph.tsx
+++ frontend/src/components/CommunicationGraph.tsx
@@ -276,15 +276,11 @@ const CommunicationGraph: React.FC<Props> = ({
                     linkCol = LINK_HIGHLIGHT_COLOR;
                     if (link.requests[0]?.type === "WS") {
                         linkCol = LINK_HIGHLIGHT_COLOR_WS;
-                    }
-                }
-
-                if (link === clickedLink || link === selectedLink) {
-                    linkCol = LINK_HIGHLIGHT_COLOR;
-                    if (link.requests[0]?.type === "QUERY" || link.requests[0]?.type === "MUTATION") {
+                    } else if (link.requests[0]?.type === "QUERY" || link.requests[0]?.type === "MUTATION") {
                         linkCol = LINK_HIGHLIGHT_COLOR_GRAPHQL;
                     }
                 }
+
                 return linkCol;
             }}
             onNodeDragEnd={(node) => {
